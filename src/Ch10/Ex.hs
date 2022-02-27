6b5113a0129ba090e006ac58310e14373b050afa
Joshua Wood <joshumcode@gmail.com>
2022-02-27 01:26:00 -0500

    finished chapter 10

--- src/Ch10/Ex.hs
+++ src/Ch10/Ex.hs
@@ -1,7 +1,11 @@
 module Ch10.Ex where
 
 import Data.Time
-    ( fromGregorian, secondsToDiffTime, UTCTime(UTCTime) )
+  ( UTCTime (UTCTime),
+    fromGregorian,
+    secondsToDiffTime,
+  )
+import Data.Foldable (maximumBy)
 
 foldr' :: (a -> b -> b) -> b -> [a] -> b
 foldr' f z [] = z
@@ -69,23 +73,32 @@ theDatabase =
     DbNumber 2004
   ]
 
+-- original implementation  was done without list comprehension
+-- filterDbDate :: [DatabaseItem] -> [UTCTime]
+-- filterDbDate [] = []
+-- filterDbDate (x : xs) = y ++ filterDbDate xs
+--   where
+--     y =
+--       case x of
+--         DbDate z -> [z]
+--         _ -> []
+
+
+-- filterDbNumber :: [DatabaseItem] -> [Integer]
+-- filterDbNumber [] = []
+-- filterDbNumber (x : xs) = y ++ filterDbNumber xs
+--   where
+--     y =
+--       case x of
+--         DbNumber z -> [z]
+--         _ -> []
+
+-- list comprehension implementation done with help from the haskell-beginners discord server
 filterDbDate :: [DatabaseItem] -> [UTCTime]
-filterDbDate [] = []
-filterDbDate (x : xs) = y ++ filterDbDate xs
-  where
-    y =
-      case x of
-        DbDate z -> [z]
-        _ -> []
+filterDbDate xs = [z | DbDate z <- xs]
 
 filterDbNumber :: [DatabaseItem] -> [Integer]
-filterDbNumber [] = []
-filterDbNumber (x : xs) = y ++ filterDbNumber xs
-  where
-    y =
-      case x of
-        DbNumber z -> [z]
-        _ -> []
+filterDbNumber xs = [z | DbNumber z <- xs]
 
 mostRecent :: [DatabaseItem] -> UTCTime
 mostRecent = maximum . filterDbDate
@@ -96,3 +109,100 @@ sumDb = sum . filterDbNumber
 avgDb :: [DatabaseItem] -> Double
 avgDb x = fromIntegral (sumDb x) / fromIntegral (length (filterDbNumber x))
 
+fibs :: [Integer]
+fibs = 1 : scanl (+) 1 fibs
+
+fibsN :: Int -> Integer
+fibsN = (!!) fibs
+
+fibs20 :: [Integer]
+fibs20 = take 20 fibs
+
+fibsLess100 :: [Integer]
+fibsLess100 = takeWhile (<100) fibs
+
+-- factorial :: Integer -> Integer
+-- factorial 0 = 1
+-- factorial n = n * factorial (n - 1)
+
+factorials :: [Integer]
+factorials = 1 : scanl (*) 1 factorials
+
+-- factorial n  = (take n+1 factorials) !! n
+
+stops :: String
+stops = "pbtdkg"
+
+vowels :: String
+vowels = "aeiou"
+
+tups :: String -> String -> [(Char, Char, Char)]
+tups [] _ = []
+tups _ [] = []
+tups (x:xs) (y:ys) = (x,y,x) : tups [x] ys ++ tups xs (y:ys)
+
+
+tupsP :: [(Char, Char, Char)]
+tupsP = filter (\(x,y,z) -> x == 'p') (tups2 stops vowels)
+
+tups2 :: [c] -> [b] -> [(c, b, c)]
+tups2 as bs = [ (a, b, a') | a <- as, b <- bs, a' <- as]
+
+seekritFunc :: Fractional a => String -> a
+-- calculates the average word length in a sentence as a string
+seekritFunc x =
+  sum (map (fromIntegral . length)  (words x))
+    / fromIntegral (length (words x))
+
+myOr :: [Bool] -> Bool
+myOr [] = False
+myOr (x:xs) = if x then True else myOr xs
+
+myOr2 :: [Bool] -> Bool
+myOr2 = foldr (\a b -> if a then True else b) False
+
+myOr3 :: [Bool] -> Bool
+myOr3 = foldr (||) False
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f = foldr (\a b -> f a || b) False 
+
+myAny' :: (a -> Bool) -> [a] -> Bool
+myAny' f x = myOr (map f x)
+
+or' :: Bool -> Bool -> Bool
+or' a b = if a then a else b
+
+and' :: Bool ->  Bool -> Bool
+and' a b = if a then b else a
+
+myElem :: Eq a => a -> [a] -> Bool
+myElem a = myAny (==a)
+
+myElem' :: Eq a => a -> [a] -> Bool
+myElem' x = foldr (\a b -> a == x || b) False
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+myReverse' :: [a] -> [a]
+myReverse' = foldr (\a b -> b ++ [a]) []
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldr (\a b -> f a : b) []
+
+squish :: [[a]] -> [a]
+squish = foldr (++) []
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f = foldr (\a b -> f a ++ b ) []
+
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f (x:xs) = foldl (\a b -> if f a b == LT then b else a) x xs
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy f (x:xs) = foldl (\a b -> if f a b == GT then b else a) x xs
