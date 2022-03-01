ad8469327f20da36f1b29df3b65292f6ab4bff6f
Joshua Wood <joshumcode@gmail.com>
2022-03-01 01:11:08 -0500

    added the rest of 11.14

--- src/Ch11/Ex.hs
+++ src/Ch11/Ex.hs
@@ -428,3 +428,52 @@ notQuite = notYet 10
 
 yusssss :: ThereYet
 yusssss = notQuite False
+
+newtype Name' = Name' String deriving Show
+newtype Acres = Acres Int deriving Show
+
+data FarmerType
+  = DairyFarmer
+  | WheatFarmer
+  | SoyBeanFarmer
+  deriving (Show)
+
+data Farmer
+  = Farmer Name' Acres FarmerType
+  deriving Show
+
+isDairyFarmer :: Farmer -> Bool
+isDairyFarmer (Farmer _ _ DairyFarmer) = True
+isDairyFarmer _ = False
+
+data FarmerRec = FarmerRec
+  { name' :: Name,
+    acres :: Acres,
+    farmerType :: FarmerType
+  }
+  deriving (Show)
+
+isDairyFarmerRec :: FarmerRec -> Bool
+isDairyFarmerRec farmer =
+  case farmerType farmer of
+    DairyFarmer -> True
+    _ -> False
+
+-- bad example, don't do this:
+-- data Automobile = Null
+--   | Car' {make :: String
+--         , model :: String
+--         , year :: Integer}
+--   deriving Show
+
+data Car' = Car'
+  { make :: String,
+    model :: String,
+    year :: Integer
+  }
+  deriving (Eq, Show)
+
+data Automobile = Null
+  | Automobile Car'
+  deriving (Eq, Show)
+
